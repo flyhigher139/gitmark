88c499bdfda728019e8aaae6e508d69d5193783b
flyhigher139 <flyhigher139@gmail.com>
2014-06-15 19:51:15 +0800

    add 4 models

--- gitmark/main/admin.py
+++ gitmark/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+admin.site.register(models.Repo)
+admin.site.register(models.RepoCreation)
+admin.site.register(models.Tag)
+admin.site.register(models.Collection)
--- gitmark/main/models.py
+++ gitmark/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Repo(models.Model):
+	name=models.CharField(max_length=200)
+	link=models.URLField()
+	starred=models.IntegerField(blank=True)
+	fork=models.IntegerField(blank=True)
+	author=models.CharField(max_length=200)
+	author_link=models.URLField()
+	last_update=models.DateTimeField(auto_now=True)
+
+class RepoCreation(models.Model):
+	user=models.ForeignKey(User)
+	repo=models.ForeignKey(Repo)
+
+class Tag(models.Model):
+	name=models.CharField(max_length=200)
+	repos=models.ManyToManyField(Repo)
+
+class Collection(models.Model):
+	name=models.CharField(max_length=200)
+	user=models.ForeignKey(User)
+	repos=models.ManyToManyField(Repo)
+	last_update=models.DateTimeField(auto_now=True)
+
+
+
+		
+		

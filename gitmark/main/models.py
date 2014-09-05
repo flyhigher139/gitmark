ed236ab2f25dc5f273e3bef45a24cd03b75a07ee
gevin <flyhigher139@gmail.com>
2014-09-05 14:43:52 +0800

    add new field to repo

--- gitmark/main/models.py
+++ gitmark/main/models.py
@@ -19,6 +19,7 @@ class Repo(models.Model):
 	subject=models.CharField(max_length=256)
 	last_update=models.DateTimeField(auto_now=True)
 	editor = models.ForeignKey(User)
+	blocked = models.BooleanField(default=False)
 
 	def __unicode__(self):
 		return self.name

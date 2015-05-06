76a1f8f826eb06be1225a475ebde5b9ae0ee7add
gevin <flyhigher139@gmail.com>
2015-05-06 11:36:01 +0800

    modify models

--- gitmark/account/models.py
+++ gitmark/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from main import models as main_models
 
 # Create your models here.
+
+class Account(models.Model):
+	user = models.OneToOneField(User)
+	avatar = models.URLField(null=True, blank=True)
+	# repos_starred = models.ForeignKey(main_models.Repo, related_name="Account_Repo")
+	# following = models.ManyToManyField(User, blank=True, null=True)
+
+class UserRepos(models.Model):
+    user = models.ForeignKey(User)
+    repo = models.ForeignKey(main_models.Repo, related_name="Account_Repo")
+
+class Collection(models.Model):
+	name = models.CharField(max_length=256)
+	user = models.ForeignKey(User)
+	repos = models.ManyToManyField(main_models.Repo)
+	last_update = models.DateTimeField(auto_now=True)
+
+# class UserAction(models.Model):
+# 	user = models.ForeignKey(User)
+# 	repo = models.ForeignKey(Repo)
+# 	action = models.CharField(max_length=256)
--- gitmark/main/models.py
+++ gitmark/main/models.py
@@ -1,25 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Language(models.Model):
 	name = models.CharField(max_length=256)
+	is_popular = models.BooleanField(default=False)
 	def __unicode__(self):
 		return self.name
 
 class Repo(models.Model):
 	name = models.CharField(max_length=256)
 	link = models.URLField()
-	starred = models.IntegerField(blank=True, null=True)
-	fork = models.IntegerField(blank=True)
+	# starred = models.IntegerField(blank=True, null=True)
+	# fork = models.IntegerField(blank=True)
 	author = models.CharField(max_length=256)
 	author_link = models.URLField(blank=True, null=True)
-	language = models.ForeignKey(Language)
-	subject = models.CharField(max_length=256)
-	last_update = models.DateTimeField(auto_now=True)
-	creator = models.ForeignKey(User)
-	blocked = models.BooleanField(default=False)
+	language = models.CharField(max_length=256)
+	desc = models.TextField()
+	# last_update = models.DateTimeField(auto_now=True)
+	# creator = models.ForeignKey(User)
+	tags = models.ManyToManyField(Tag, blank=True, null=True)
+	blocked = models.BooleanField(default=False) #administrator use this filed for repo management
 
 	def __unicode__(self):
 		return self.name
@@ -30,26 +32,8 @@ class Repo(models.Model):
 #
 class Tag(models.Model):
 	name = models.CharField(max_length=256)
-	repos = models.ManyToManyField(Repo, blank=True, null=True)
-	counts = models.IntegerField(default=0)
+	# counts = models.IntegerField(default=0)
 
-class TagStatistic(models.Model):
-	tag = models.ForeignKey(Tag)
-	repo_count = models.IntegerField(default=0)
-
-class Collection(models.Model):
-	name = models.CharField(max_length=256)
-	user = models.ForeignKey(User)
-	repos = models.ManyToManyField(Repo)
-	last_update = models.DateTimeField(auto_now=True)
-
-class UserAction(models.Model):
-	user = models.ForeignKey(User)
-	repo = models.ForeignKey(Repo)
-	action = models.CharField(max_length=256)
-
-class Account(models.Model):
-	user = models.ForeignKey(User, related_name="Account_User")
-	avatar = models.URLField(null=True, blank=True)
-	repos_starred = models.ForeignKey(Repo, related_name="Account_Repo")
-	following = models.ManyToManyField(User, blank=True, null=True)
+# class TagStatistic(models.Model):
+# 	tag = models.ForeignKey(Tag)
+# 	repo_count = models.IntegerField(default=0)

1188b9c8393b8db160720e0c031136513c54f35d
gevin <flyhigher139@gmail.com>
2015-09-02 22:44:28 +0800

    env file

---
+++ gitmark/gitmark/env.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+github = {
+	'client_id': '21d8d7fc0a819d3d0e2f',
+	'client_secret': 'ed763aebe1925f8f70fb5aa3c80fcb00a26a7680',
+}
+
+email = {
+	'username': 'gmail',
+	'password': 'password',
+}

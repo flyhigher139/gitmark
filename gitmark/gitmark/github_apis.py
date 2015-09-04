8dddbe1441c5112a4a3f770ff2a9a5c311e9ba05
gevin <flyhigher139@gmail.com>
2015-09-04 16:00:03 +0800

    project struct

--- gitmark/gitmark/github_apis.py
+++ gitmark/gitmark/github_apis.py
@@ -4,11 +4,11 @@
 base_url = 'https://api.github.com'
 
 def auth_user():
-	api = '/user'
-	url = base_url + api
-	return url
+    api = '/user'
+    url = base_url + api
+    return url
 
 def starred_repos(username):
-	api = '/users/{0}/starred'.format(username)
-	url = base_url + api
+    api = '/users/{0}/starred'.format(username)
+    url = base_url + api
     return url
---
+++ gitmark/gitmark/settings/env.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+github = {
+	'client_id': '22d8d8fc9a819d3d0e2f',
+	'client_secret': 'ed763aebe5695f8f70fb5aa3c80fcb00a36a7680',
+	'app_user': 'gevinyu',
+	'app_pass': 'fly_8414194',
+}
+
+email = {
+	'username': 'gmail',
+	'password': 'password',
+}

e0ace652693236739437cfbf0bec8d69d9665bc7
Gevin <flyhigher139@gmail.com>
2017-06-28 20:57:12 +0800

    Update env.py

--- gitmark/gitmark/settings/env.py
+++ gitmark/gitmark/settings/env.py
@@ -4,8 +4,8 @@
 github = {
 	'client_id': '22d8d8fc9a819d3d0e2f',
 	'client_secret': 'ed763aebe5695f8f70fb5aa3c80fcb00a36a7680',
-	'app_user': 'gevinyu',
-	'app_pass': 'fly_8414194',
+	'app_user': '',
+	'app_pass': '',
 }
 
 email = {
@@ -14,8 +14,8 @@ email = {
 }
 
 qiniu = {
-	'access_key': 'pJJ-Ry9tWvmlaGUhNNBcJopEziAyl_w6kjnYdolU',
-	'secret_key': 'AZYqrg4_sBHzUb8hkChNgynU350QYXeJ1W8lFvJs',
+	'access_key': '',
+	'secret_key': '',
 	'bucket_name': 'development',
 	'base_url': 'http://7xn70j.com1.z0.glb.clouddn.com/',
 }
